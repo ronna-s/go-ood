6bfb0747360c3e6bc95f9b50f8190a8c6188ff04
Ronna Steinberg <ronna-s@users.noreply.github.com>
2022-07-07 08:55:06 +0200

    Remove Solution

--- cmd/habitat/habitat.go
+++ cmd/habitat/habitat.go
@@ -22,22 +22,4 @@ type Habitat struct {
 
 // Add adds an animal to the habitat
 func (h *Habitat) Add(a animal.Animal) string {
-	for i, a2 := range h.Animals {
-		if p, ok := a.(animal.Predator); ok {
-			if p.Eats(a2) && !p.Full() {
-				p.Eat(a2)
-				h.Animals = append(h.Animals[:i], h.Animals[i+1:]...)
-				h.Animals = append(h.Animals, a)
-				return p.MakeSound()
-			}
-		}
-		if p, ok := a2.(animal.Predator); ok {
-			if p.Eats(a) && !p.Full() {
-				p.Eat(a)
-				return a.MakeSound()
-			}
-		}
-	}
-	h.Animals = append(h.Animals, a)
-	return a.MakeSound()
 }
