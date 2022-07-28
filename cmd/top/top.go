1a0fe70357f0a7cf185b3a0fc78262a50a474dae
Ronna Steinberg <ronnas@gmail.com>
2022-07-28 13:55:02 +0200

    top

--- cmd/top/top.go
+++ cmd/top/top.go
@@ -10,50 +10,48 @@ import (
 )
 
 type (
-	Blogger struct {
-		Name  string
-		Reads int
+	Artist struct {
+		Name    string
+		Listens int
 	}
-	Post struct {
-		Name  string
-		Reads int
+	Song struct {
+		Name    string
+		Listens int
 	}
 )
 
-// Less ... see https://stackoverflow.com/a/70361353/2730407
-func (b1 Blogger) Less(b2 Blogger) bool {
-	return b1.Reads > b2.Reads
+func (b1 Artist) Less(b2 Artist) bool {
+	return b1.Listens > b2.Listens
 }
 
-// Less ... see https://stackoverflow.com/a/70361353/2730407
-func (p1 Post) Less(p2 Post) bool {
-	return p1.Reads > p2.Reads
+func (p1 Song) Less(p2 Song) bool {
+	return p1.Listens > p2.Listens
 }
 
 func main() {
 	var (
-		bloggers []Blogger
-		posts    []Post
+		bloggers []Artist
+		posts    []Song
 	)
 	for i := 0; i < rand.Intn(1000)+1000; i++ {
-		bloggers = append(bloggers, Blogger{Name: namegen.Generate(), Reads: rand.Intn(851202)})
+		bloggers = append(bloggers, Artist{Name: namegen.Generate(), Listens: rand.Intn(851202)})
 	}
 
 	for i := 0; i < rand.Intn(1000)+1000; i++ {
-		posts = append(posts, Post{Name: namegen.Generate(), Reads: rand.Intn(800917)})
+		posts = append(posts, Song{Name: namegen.Generate(), Listens: rand.Intn(800917)})
 	}
 
 	hblogger := heap.New(bloggers)
 	hposts := heap.New(posts)
-	fmt.Println(withColor(cyan, "Our top 10 bloggers are:"))
+	fmt.Println(withColor(cyan, "Our top 10 artists are:"))
 	fmt.Println(withColor(cyan, "========================"))
 	for i := 0; i < 10; i++ {
-		fmt.Printf("%d: %s with %d reads\n", i+1, hblogger.Pop().Name, hposts.Pop().Reads)
+		fmt.Printf("%d: %s with %d reads\n", i+1, hblogger.Pop().Name, hposts.Pop().Listens)
 	}
-	fmt.Println(withColor(purple, "Our top 10 posts are:"))
-	fmt.Println(withColor(purple, "====================="))
+	fmt.Println(withColor(purple, "Our top of the pop songs are:"))
+	fmt.Println(withColor(purple, "============================="))
 	for i := 0; i < 10; i++ {
-		fmt.Printf("%d: %s with %d reads\n", i+1, hposts.Pop().Name, hposts.Pop().Reads)
+		fmt.Printf("%d: %s with %d reads\n", i+1, hposts.Pop().Name, hposts.Pop().Listens)
 	}
 }
 
