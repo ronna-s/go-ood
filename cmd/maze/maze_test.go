4c2751367240a8527c30cd0604ae550884717fb3
ronna-s <ronna.steinberg@gmail.com>
2022-10-05 09:17:53 -0500

    add timeout to test to handle docker ctrl+c issues

--- cmd/maze/maze_test.go
+++ cmd/maze/maze_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/mock"
 
@@ -10,57 +12,68 @@ import (
 )
 
 func TestSolveMaze(t *testing.T) {
-	t.Run("test finished", func(t *testing.T) {
-		g := mocks.Gopher{}
-		g.On("Finished").Return(true)
-		SolveMaze(&g)
-		g.AssertExpectations(t)
-	})
-
-	// Test that the solution can solve any type of maze with any amount of dimensions
-	t.Run("basic navigation", func(t *testing.T) {
-		const (
-			dims  = 15
-			lefts = 12
-		)
-		g := mocks.Gopher{}
-		nLefts := 0
-		nRights := 0
-		moved := false
-		canMove := false
-		g.On("Finished").Return(func() bool {
-			return moved
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	var finished bool
+	go func() {
+		t.Run("test finished", func(t *testing.T) {
+			g := mocks.Gopher{}
+			g.On("Finished").Return(true)
+			SolveMaze(&g)
+			g.AssertExpectations(t)
 		})
 
-		g.On("TurnRight").Run(func(args mock.Arguments) {
-			nRights++
-			if nRights > nLefts && nRights-nLefts == dims-lefts {
-				canMove = true
-			} else if nLefts > nRights && nLefts-nRights == lefts {
-				canMove = true
-			} else {
-				canMove = false
-			}
-		}).Maybe()
-		g.On("TurnLeft").Run(func(args mock.Arguments) {
-			nLefts++
-			if nRights > nLefts && nRights-nLefts == 1 {
-				canMove = true
-			} else if nLefts > nRights && nLefts-nRights == 3 {
-				canMove = true
-			} else {
-				canMove = false
-			}
-		}).Maybe()
-		g.On("Move").Return(func() error {
-			if !canMove {
-				return errors.New("can't move")
-			}
-			moved = true
-			return nil
-		})
+		// Test that the solution can solve any type of maze with any amount of dimensions
+		t.Run("basic navigation", func(t *testing.T) {
+			const (
+				dims  = 15
+				lefts = 12
+			)
+			g := mocks.Gopher{}
+			nLefts := 0
+			nRights := 0
+			moved := false
+			canMove := false
+			g.On("Finished").Return(func() bool {
+				return moved
+			})
 
-		SolveMaze(&g)
-		g.AssertExpectations(t)
-	})
+			g.On("TurnRight").Run(func(args mock.Arguments) {
+				nRights++
+				if nRights > nLefts && nRights-nLefts == dims-lefts {
+					canMove = true
+				} else if nLefts > nRights && nLefts-nRights == lefts {
+					canMove = true
+				} else {
+					canMove = false
+				}
+			}).Maybe()
+			g.On("TurnLeft").Run(func(args mock.Arguments) {
+				nLefts++
+				if nRights > nLefts && nRights-nLefts == 1 {
+					canMove = true
+				} else if nLefts > nRights && nLefts-nRights == 3 {
+					canMove = true
+				} else {
+					canMove = false
+				}
+			}).Maybe()
+			g.On("Move").Return(func() error {
+				if !canMove {
+					return errors.New("can't move")
+				}
+				moved = true
+				return nil
+			})
+
+			SolveMaze(&g)
+			g.AssertExpectations(t)
+		})
+		finished = true
+		cancel()
+	}()
+	<-ctx.Done()
+	if !finished {
+		t.Fatal("test timed out")
+	}
 }
