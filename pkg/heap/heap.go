60c38a5b3c2dac27f28bca542a7049dde7cdc6ff
ronna-s <ronna.steinberg@gmail.com>
2023-06-27 16:46:34 +0200

    remove heap implementation

--- pkg/heap/heap.go
+++ pkg/heap/heap.go
@@ -2,12 +2,6 @@
 // By providing a generic heap we can avoid messy interface conversions and provide a friendly interface
 package heap
 
-func New[T Ordered[T]](s []T) Heap[T] {
-	h := Heap[T](s)
-	h.Init()
-	return h
-}
-
 func (h Heap[T]) Init() {
 	n := len(h)
 	for i := n/2 - 1; i >= 0; i-- {
